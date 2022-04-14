3ebfa8e7df33343c6601f815958adaf78f4ddaed
DaveMeador <92818043+DaveMeador@users.noreply.github.com>
2022-04-14 13:51:17 -0500

    Add files via upload

---
+++ src/newAccount.js
@@ -0,0 +1,6 @@
+function accountNumber() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "number");
+    x.setAttribute("value", "");
+    document.body.appendChild(x);
+  }
